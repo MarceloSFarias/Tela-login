ecc2d0b8a24a3ccef5220691e59bb1014765fdc4
Nekiol <nekiol@hotmail.com>
2023-06-21 22:37:34 -0300

    AFS

---
+++ app/src/main/java/com/nekiol/br/loginapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.nekiol.br.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.nekiol.br.loginapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEntrar.setOnClickListener {
+            val username = binding.editUsername.text.toString().trim()
+            val password = binding.editPassword.text.toString().trim()
+
+            if (username.equals("user") && password.equals("pass")){
+                Toast.makeText(applicationContext, "Login OK", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "Login Inválido", Toast.LENGTH_SHORT).show()
+            }
+
+            binding.editUsername.setText("")
+            binding.editPassword.setText("")
+        }
+    }
+}
